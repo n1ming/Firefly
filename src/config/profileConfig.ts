f01f9a5a7f355e535180649200cb4d8f65990cb6
n1ming <2208564278@qq.com>
2026-05-19 18:49:03 +0800

    更新内容

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -6,13 +6,13 @@ export const profileConfig: ProfileConfig = {
 	// 1. public 目录（以 "/" 开头，不优化）："/assets/images/avatar.webp"
 	// 2. src 目录（不以 "/" 开头，自动优化但会增加构建时间，推荐）："assets/images/avatar.webp"
 	// 3. 远程 URL："https://example.com/avatar.jpg"
-	avatar: "assets/images/avatar.avif",
+	avatar: "https://pic1.imgdb.cn/item/689571d058cb8da5c8111d78.webp",
 
 	// 名字
-	name: "Firefly",
+	name: "n1ming",
 
 	// 个人签名
-	bio: "Hello, I'm Firefly.",
+	bio: "你好，我是n1ming",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
@@ -30,7 +30,7 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "GitHub",
 			icon: "fa7-brands:github",
-			url: "https://github.com/CuteLeaf",
+			url: "https://github.com/n1ming",
 			showName: false,
 		},
 		{
---
+++ src/content/posts/hello/Blog 开始！.md
@@ -0,0 +1,37 @@
+---
+
+title: 第一篇博客
+
+published: 2026-05-19
+
+description: 这是一个使用Astro框架和Firefly主题的博客，感谢开源精神。其次，本博客部署在cloudflare，使用自己的域名，或许需要魔法访问。
+
+image: ./go.png
+
+tags: [说说]
+
+category: 说说
+
+draft: false
+
+author: n1ming
+
+---
+
+# Blog 开始！
+
+> 本博客，主要是用Astro框架和Firefly主题，感谢大佬开源精神。
+> 
+> 其次，主要部署在cloudflare上，并使用自己的域名。如果卡顿，请使用魔法。
+> 
+> 最后，本博客主要分享python，go脚本，或许还会js，Java？？？也算作为自己的
+> 
+> 一个日常笔记了。顺便可能会还挂载一些工具（随心情）
+
+
+
+那么，GO！
+
+![python and go](./python_go.png)
+
+
